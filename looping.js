1ca94d721e1b9107017b5f4335c5fc61ee99ffa7
tawakalmit <tawakal.mit@gmail.com>
2022-06-29 14:24:15 +0700

    add for explanation

--- looping.js
+++ looping.js
@@ -1,4 +1,11 @@
 // Looping
+// for merupakan kata kunci untuk melakukan perulangan
+// blok kode yang terdapat dalam for akan selallu diulangi selaman kondisi for terpenuhi
+
+for(init statement; kondisi; post statement){
+	// masukkan kode perulangan
+}
+
 for(let i = 0; i <= 5; i++){
 	console.log(i);
 }
