61ae9554df5cb6e9461adb38268fbb9653afaf04
tawakalmit <tawakal.mit@gmail.com>
2022-06-29 14:28:13 +0700

    add while explanation

--- looping.js
+++ looping.js
@@ -2,10 +2,20 @@
 // for merupakan kata kunci untuk melakukan perulangan
 // blok kode yang terdapat dalam for akan selallu diulangi selaman kondisi for terpenuhi
 
-for(init statement; kondisi; post statement){
-	// masukkan kode perulangan
-}
+// for(init statement; kondisi; post statement){
+// 	// masukkan kode perulangan
+// }
 
 for(let i = 0; i <= 5; i++){
 	console.log(i);
+}
+
+// While loop
+// Merupakan versi peulangan yang lebih sederhana dibanding for loop
+// Hanya terdapat kondisi perulangan tanpa ada init statement dan post statement
+
+let counter = 1;
+while (counter<= 10) {
+	console.log(counter);
+	counter++
 }
