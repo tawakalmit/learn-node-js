50902ad00860e77e330d9e0ffea0f78a6cf1a425
tawakalmit <tawakal.mit@gmail.com>
2022-06-29 10:31:56 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,5 @@
+let simpleSum = (a, b) => {
+	return a+b
+}
+
+console.log(simpleSum(5,6));
---
+++ object.js
@@ -0,0 +1,20 @@
+// contoh array
+let strawHat = ["Luffy", "Zoro", "Nami", "Usopp", "Sanji", "Chopper", "Robin", "Franky", "Brook", "Jinbe"]
+
+console.log(strawHat);
+console.table(strawHat);
+
+// contoh object
+let mugiwara = {
+	captain: "Monkey D Luffy",
+	viceCaptain: "Rorono Zoro",
+	navigator: "Nami",
+	sniper: "Usopp",
+	chef: "Vinsmoke Sanji",
+	doctor: "Tony Tony Chopper",
+	archeolog: "Nico Robin",
+	mechanic: "Cyborg Franky",
+	musician: "Brook",
+	helmsman: "Jinbe"
+}
+
